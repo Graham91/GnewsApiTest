9c00c62551b3f28990836537c134fcf25eb941cb
grambo <gramham@live.com>
2020-08-06 15:49:07 -0600

    finished setting up API

---
+++ index.js
@@ -0,0 +1,111 @@
+var obj = JSON.parse([
+  {
+    title:
+      "LeBron James out vs. Rockets Thursday with groin soreness; Russell Westbrook unlikely to play, per report",
+    description:
+      "LeBron James will miss Thursday's game between the Los Angeles Lakers and Houston Rockets, coach Frank Vogel revealed on Thursday. He is apparently dealing with soreness to his right groin. In ...",
+    url:
+      "https://www.cbssports.com/nba/news/lebron-james-out-vs-rockets-thursday-with-groin-soreness-russell-westbrook-unlikely-to-play-per-report/",
+    image: "https://images.gnews.io/7d067454f5cecbf10daef155a75cdc3e",
+    publishedAt: "2020-08-06 12:47:00 UTC",
+    source: { name: "CBSSports.com", url: "https://www.cbssports.com" },
+  },
+  {
+    title: "LeBron James Is Out Against The Houston Rockets",
+    description:
+      "LeBron James will miss Thursday's game against the Houston Rockets because of a sore right groin. James missed 17-straight games last season after suffering a right groin injury i ...",
+    url:
+      "https://www.si.com/nba/lakers/news/lebron-james-is-out-against-the-houston-rockets",
+    image: "https://images.gnews.io/a4665c92ed7f3b875a16fa28a62df62d",
+    publishedAt: "2020-08-06 12:21:00 UTC",
+    source: { name: "Sports Illustrated", url: "https://www.si.com" },
+  },
+  {
+    title:
+      "LeBron James says Lakers have off-court issues, out vs. Rockets (groin)",
+    description:
+      "LeBron James said the Los Angeles Lakers have off-court issues. He will not play against the Houston Rockets due to a groin injury.",
+    url:
+      "https://www.msn.com/en-us/sports/nba/lebron-james-says-lakers-have-off-court-issues-out-vs-rockets-groin/ar-BB17ELBo",
+    image: "https://images.gnews.io/f6817a2622fe3d15ba297025e304b7c8",
+    publishedAt: "2020-08-06 12:18:03 UTC",
+    source: { name: "NBC Sports on MSN.com", url: "https://www.msn.com" },
+  },
+  {
+    title: "Lakers Release Concerning Injury News on LeBron James",
+    description:
+      "The Los Angeles Lakers released some concerning injury news on LeBron James on Thursday ahead of a matchup with the Houston Rockets.",
+    url:
+      "https://heavy.com/sports/los-angeles-lakers/2020/08/lebron-james-groin-injury-out/",
+    image: "https://images.gnews.io/55ffe17784d069444e601f436318572e",
+    publishedAt: "2020-08-06 11:58:00 UTC",
+    source: { name: "Heavy.com", url: "https://heavy.com" },
+  },
+  {
+    title: "Lakers star LeBron James will not play against Rockets",
+    description:
+      "LeBron James will not play against the Houston Rockets on Thursday night because of a sore right groin, according to the Lakers.",
+    url:
+      "https://www.msn.com/en-us/sports/nba/lakers-star-lebron-james-will-not-play-against-rockets/ar-BB17ECNj",
+    image: "https://images.gnews.io/0a2cdf9fb2819bf7888f640c876328bd",
+    publishedAt: "2020-08-06 11:57:37 UTC",
+    source: {
+      name: "Los Angeles Times on MSN.com",
+      url: "https://www.msn.com",
+    },
+  },
+  {
+    title:
+      "Lakers' LeBron James will miss game against Rockets with sore groin",
+    description:
+      "The Lakers, who have the No. 1 seed in the Western Conference secured, are 2-2 in the NBA bubble and among the worst offensive teams in the restart.",
+    url:
+      "https://www.usatoday.com/story/sports/nba/lakers/2020/08/06/lebron-james-will-miss-rockets-game-with-sore-right-groin/3311316001/",
+    image: "https://images.gnews.io/1007f892288cbc65bba17d00cb51dbda",
+    publishedAt: "2020-08-06 11:54:00 UTC",
+    source: { name: "USA Today", url: "https://www.usatoday.com" },
+  },
+  {
+    title:
+      "LeBron James out with sore right groin for Thursday's Lakers-Rockets matchup",
+    description:
+      "The Los Angeles Lakers need to right their ship after a 105-86 drubbing at the hands of the Oklahoma City Thunder, but LeBron James won’t be able to help them do it on Thursday. The Lakers announced ...",
+    url:
+      "https://sports.yahoo.com/le-bron-james-out-with-sore-right-groin-for-thursdays-lakers-rockets-matchup-181433803.html",
+    image: "https://images.gnews.io/005d9498675ecf5c708714456e4df1d8",
+    publishedAt: "2020-08-06 11:25:00 UTC",
+    source: { name: "YAHOO!", url: "https://sports.yahoo.com" },
+  },
+  {
+    title:
+      "LeBron James discusses 'off-court' issues outside of Lakers control",
+    description:
+      "LeBron James won’t play tonight against the Houston Rockets, but him resting in the final back-to-back the Lakers have this season isn’t nearly as interesting as what he said last night to reporters ...",
+    url:
+      "https://lebronwire.usatoday.com/2020/08/06/lebron-james-discusses-off-court-issues-outside-of-lakers-control/",
+    image: "https://images.gnews.io/1e05c7db9d33aed93d909235f30156ee",
+    publishedAt: "2020-08-06 11:22:00 UTC",
+    source: { name: "USA Today", url: "https://lebronwire.usatoday.com" },
+  },
+  {
+    title: "LeBron James held out of tonight's game vs. Houston Rockets",
+    description:
+      "The Los Angeles Lakers are not going to have LeBron James play in the second game of a back-to-back tonight against Houston.",
+    url:
+      "https://lebronwire.usatoday.com/2020/08/06/lebron-james-held-out-of-tonights-game-vs-houston-rockets/",
+    image: "https://images.gnews.io/4d1466069338426eb4d0e07d20c360a3",
+    publishedAt: "2020-08-06 11:08:00 UTC",
+    source: { name: "USA Today", url: "https://lebronwire.usatoday.com" },
+  },
+  {
+    title: "LeBron James won't play against Rockets on Thursday night",
+    description:
+      "LeBron James will sit out Thursday night's game against the Rockets to rest a sore groin, the Rockets announced Thursday afternoon.",
+    url:
+      "https://www.chron.com/sports/rockets/article/Lakers-LeBron-James-wont-play-against-Rockets-15464452.php",
+    image: "https://images.gnews.io/c6aa3191b053ef7733bdf4bf5cde618b",
+    publishedAt: "2020-08-06 11:08:00 UTC",
+    source: { name: "Houston Chronicle", url: "https://www.chron.com" },
+  },
+]);
+console.log(obj);
